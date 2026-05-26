76c50bbc3ec48159f9f5e9b1e01cb6f14053dead
aiera2876 <aiera2876@gmail.com>
2026-05-26 14:53:09 +0545

    added appsearchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import SearchAppBar from "./components/SearchAppBar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <SearchAppBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Product />} />
       </Routes>
+
     </BrowserRouter>
   );
-  
 }
